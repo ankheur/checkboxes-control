eb5e88aa0660ad40b4bdddfa1ea65e7c575d9246
Anne-Marie Serveto <servetonguyen@outlook.com>
2021-10-06 21:04:31 +0200

    Changes in var names to const and let names + translation in French for the commentary

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 /*  
     the first entry is the id of each checkboxes, the followings are the checkboxes which should be disabled 
-    if this checkbox is checked 
+    if this checkbox is checked || La première entrée est l'id de chaque checkboxes, les suivantes sont les id des checkboxes qui seront désactivées si cette checkboxe est active.
 */
 
-var plage = [['A1', 'allCheckbox'], 
+const plage = [['A1', 'allCheckbox'], 
              ['A2', 'allCheckbox'], 
              ['A3', 'A4'], 
              ['A4', 'A3'], 
@@ -15,43 +15,43 @@ var plage = [['A1', 'allCheckbox'],
              ['A8', 'A7']];
 
 
-var inputs = document.querySelectorAll('#testForm input');
-var checkboxes = document.querySelectorAll('#testForm input[type=checkbox]');
+const inputs = document.querySelectorAll('#testForm input');
+const checkboxes = document.querySelectorAll('#testForm input[type=checkbox]');
 
 
 inputs.forEach(function (item) {
 
     item.addEventListener('click', function () {
 
-        var checked = verifChecked();
-        var itemDisabled = [];
+        let checked = verifChecked();
+        let itemDisabled = [];
 
-        //On commence par tout décocher
+        //On commence par tout décocher || We start with checking everything
         inputs.forEach(function (input) {
             input.disabled = false;
         });
 
-        //If there is nothing to check, exit the function
+        //If there is nothing to check, exit the function || s'il n'y a rien à cocher, ça sort de la fonction
         if (checked.length === 0) {
             return;
         }
 
 
-        //For every checked items
+        //For every checked items || Pour chaque items checkés
         checked.forEach(function (idChecked) {
 
-            // We look in the arrays
-            for (var i = 0; i < plage.length; i++) {
+            // We look in the arrays || On regarde dans les tableaux
+            for (const i = 0; i < plage.length; i++) {
 
-                var plageID = plage[i][0];
+                const plageID = plage[i][0];
 
-                //Quand on trouve la bonne plage
+                //Quand on trouve la bonne plage || When you find the right one
                 if (plageID === idChecked) {
 
-                    //we get the ids in the array (expect the first one)
-                    for (var j = 1; j < plage[i].length; j++) {
+                    //we get the ids in the array (expect the first one) || On trouve les ids du tableaux sauf pour le premier
+                    for (const j = 1; j < plage[i].length; j++) {
                     
-                        //we push them in a new array
+                        //we push them in a new array || on les push dans un nouveau tableau
                         itemDisabled.push(plage[i][j]);
                         
                     }
@@ -59,7 +59,7 @@ inputs.forEach(function (item) {
             }
         });
 
-        //If a checkbox should disable all the others (with the keyword allCheckbox)
+        //If a checkbox should disable all the others (with the keyword allCheckbox) || Si une checkbox doit désactiver toutes les autres avec le mot-clé allCheckbox
         if (itemDisabled.includes('allCheckbox')) {
             checkboxes.forEach(function (checkbox) {
                 checkbox.disabled = true;
@@ -69,19 +69,19 @@ inputs.forEach(function (item) {
             item.checked = true;
 
             
-        } else { //Else we disable the inputs contained in the array itemDisabled
+        } else { //Else we disable the inputs contained in the array itemDisabled || ou on désactive les inputs contenus dans le tableau itemDisabled
             itemDisabled.forEach(function (id) {
-                var inputToDisable = document.getElementById(id);
+                const inputToDisable = document.getElementById(id);
                 inputToDisable.disabled = true;
             });
         }
     });
 });
 
-//Return an array with all checked items
+//Return an array with all checked items || Retourne un tableau avec les items checkés.
 function verifChecked() {
-    var checkedInputs = document.querySelectorAll('#testForm input:checked');
-    var checkedID = [];
+    const checkedInputs = document.querySelectorAll('#testForm input:checked');
+    const checkedID = [];
     checkedInputs.forEach(function (item) {
         checkedID.push(item.id);
     });
